d6f426ed748b7017c6d0ad1e8407b2726236eb1c
Uros Jevtic <uros.jevtic888@gmail.com>
2023-03-08 20:12:58 +0100

    Accommodation registration functining

---
+++ InitialProject/InitialProject/Model/Accommodation.cs
@@ -0,0 +1,67 @@
+﻿using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{   
+    public enum AccommodationType { appartment, house, cabin}
+    public class Accommodation : ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public AccommodationType Type { get; set; }
+        public int MaxGuests { get; set; }
+        public int MinReservationDays { get; set; }
+        public int CancelationPeriod { get; set; }
+        public List<String> Images { get; set; }
+
+        
+        public Accommodation() { }
+
+        public Accommodation(int id, string name, string location, AccommodationType type, int maxGuests, int minReservationDays, int cancelationPeriod, List<string> images)
+        {
+            Id = id;
+            Name = name;
+            Location = location;
+            Type = type;
+            MaxGuests = maxGuests;
+            MinReservationDays = minReservationDays;
+            CancelationPeriod = cancelationPeriod;
+            Images = images;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Name, Location, Type.ToString(), MaxGuests.ToString(), MinReservationDays.ToString(), CancelationPeriod.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+            Location = values[2];
+            switch(values[3])
+            {
+                case "appartment":
+                    Type = AccommodationType.appartment;
+                    break;
+                case "house":
+                    Type = AccommodationType.house; 
+                    break;
+                case "cabin":
+                    Type = AccommodationType.cabin;
+                    break;
+            }
+            MaxGuests = Convert.ToInt32(values[4]);
+            MinReservationDays = Convert.ToInt32(values[5]);
+            CancelationPeriod = Convert.ToInt32(values[6]);
+
+        }
+
+    }
+}
---
+++ InitialProject/InitialProject/Repository/AccommodationRepository.cs
@@ -0,0 +1,33 @@
+﻿using InitialProject.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repository
+{
+    public class AccommodationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/accommodation.csv";
+
+        private readonly Serializer<Accommodation> _serializer;
+
+        private List<Accommodation> _accommodations;
+
+        public AccommodationRepository()
+        {
+            _serializer = new Serializer<Accommodation>();
+            _accommodations = _serializer.FromCSV(FilePath);
+        }
+
+        public void Save(Accommodation accommodation)
+        {
+            accommodation.Id = 1;
+            _accommodations = _serializer.FromCSV(FilePath);
+            _accommodations.Add(accommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+    }
+}
