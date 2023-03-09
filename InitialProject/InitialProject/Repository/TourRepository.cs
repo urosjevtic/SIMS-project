1fd9a3e4c382ab614fdfd6320f75c4a18ce12321
Dajana00 <dajanaskocajic18@gmail.com>
2023-03-09 22:37:19 +0100

    Klase Image i Locations

---
+++ InitialProject/InitialProject/Repository/TourRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Model;
+using InitialProject.Serializer;
+
+namespace InitialProject.Repository
+{
+    internal class TourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tours.csv";
+
+        private readonly Serializer<Tour> _serializer;
+
+        private List<Tour> _tours;
+
+        public TourRepository()
+        {
+            _serializer = new Serializer<Tour>();
+            _tours = _serializer.FromCSV(FilePath);
+        }
+
+       
+    }
+}
