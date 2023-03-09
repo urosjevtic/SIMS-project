00436827ec976a7f4b676f0ecd0f3c3586826069
Uros Jevtic <uros.jevtic888@gmail.com>
2023-03-09 22:09:59 +0100

    Added user login role

--- InitialProject/InitialProject/View/SignInForm.xaml.cs
+++ InitialProject/InitialProject/View/SignInForm.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using InitialProject.Forms;
 using InitialProject.Model;
 using InitialProject.Repository;
+using InitialProject.View;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -50,8 +51,11 @@ namespace InitialProject
             {
                 if(user.Password == txtPassword.Password)
                 {
-                    CommentsOverview commentsOverview = new CommentsOverview(user);
-                    commentsOverview.Show();
+                    if(user.Role == UserRole.Owner)
+                    {
+                        //OwnerMainWindow ownerMainWindow = new OwnerMainWindow();
+                        //ownerMainWindow.Show();
+                    }
                     Close();
                 } 
                 else
