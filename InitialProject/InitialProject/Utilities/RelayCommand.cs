1fb4ce36f29afbac32ebd8a289f930f41efedd02
Uros Jevtic <uros.jevtic888@gmail.com>
2023-04-08 15:01:54 +0200

    New owner main window

---
+++ InitialProject/InitialProject/Utilities/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace InitialProject.Utilities
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ InitialProject/InitialProject/ViewModels/OwnerMainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using InitialProject.Model;
+using InitialProject.Utilities;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using InitialProject.Domain.Model;
+
+namespace InitialProject.ViewModel
+{
+    public class OwnerMainViewModel : INotifyPropertyChanged
+    {
+        private User _loggedInUser;
+
+        public User LogedInUser
+        {
+            get { return _loggedInUser; }
+            set
+            {
+                _loggedInUser = value;
+                OnPropertyChanged(nameof(LogedInUser));
+            }
+        }
+
+        public string WelcomeMessage => "Welcome";
+
+        private Visibility _sideScreenVisibility = Visibility.Visible;
+
+        public Visibility SideScreenVisibility
+        {
+            get { return _sideScreenVisibility; }
+            set
+            {
+                _sideScreenVisibility = value;
+                OnPropertyChanged(nameof(SideScreenVisibility));
+            }
+        }
+
+        private Visibility _mainScreenVisibility = Visibility.Collapsed;
+
+        public Visibility MainScreenVisibility
+        {
+            get { return _mainScreenVisibility; }
+            set
+            {
+                _mainScreenVisibility = value;
+                OnPropertyChanged(nameof(MainScreenVisibility));
+            }
+        }
+
+        public ICommand BurgerBarClosedCommand => new RelayCommand(BurgerBarClosed);
+        public ICommand BurgerBarOpenCommand => new RelayCommand(BurgerBarOpen);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public OwnerMainViewModel(User user)
+        {
+            LogedInUser = user;
+        }
+
+        private void BurgerBarOpen()
+        {
+            SideScreenVisibility = Visibility.Visible;
+            MainScreenVisibility = Visibility.Collapsed;
+        }
+
+        private void BurgerBarClosed()
+        {
+            SideScreenVisibility = Visibility.Collapsed;
+            MainScreenVisibility = Visibility.Visible;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
