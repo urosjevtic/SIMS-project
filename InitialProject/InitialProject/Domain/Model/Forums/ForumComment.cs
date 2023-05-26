be309d6b49f39bf1ce00ce1890928747cfd7131e
Uros Jevtic <uros.jevtic888@gmail.com>
2023-05-26 21:59:43 +0200

    Forum comments

---
+++ InitialProject/InitialProject/Domain/Model/Forums/ForumComment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Domain.Model.Users;
+using InitialProject.Serializer;
+
+namespace InitialProject.Domain.Model.Forums
+{
+    public class ForumComment : ISerializable
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public User Author { get; set; }
+
+        public int NumberOfReports { get; set; }
+
+        public ForumComment() { }
+
+        public ForumComment(int id, string comment, User author, int numberOfReports)
+        {
+            Id = id;
+            Comment = comment;
+            Author = author;
+            NumberOfReports = numberOfReports;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Comment, Author.Id.ToString(), NumberOfReports.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Comment = values[1];
+            Author.Id = Convert.ToInt32(values[2]);
+            NumberOfReports = Convert.ToInt32(values[3]);
+
+        }
+    }
+
+}
